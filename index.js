0e0ae829f64ad7a85ff103c154f87d209fceaee0
everyoneselectronic <fogertyliam@gmail.com>
2018-01-28 13:11:22 +0000

    3

--- index.js
+++ index.js
@@ -72,11 +72,6 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
-// set the home page route
-// app.get('/', (request, response) => {
-//     // ejs render automatically looks in the views folder
-//     response.render('index.html');
-// });
 
 app.listen(port, () => {
     // will echo 'Our app is running on http://localhost:5000 when run locally'
@@ -89,86 +84,86 @@ setInterval(() => {
 }, 900000);
 
 
-function makeAudioObject(a) {
-    var str = baseUrl + a + ".wav]";
-    var ids = [str];
+// function makeAudioObject(a) {
+//     var str = baseUrl + a + ".wav]";
+//     var ids = [str];
 
-    // console.log(ids);
+//     // console.log(ids);
 
-    var audio = {
-        "name" : "PlayFiles",
-        "ids" :  ids,
-        "locale" : "en-US"
-    }
-    responsedata['Instructions'].push(audio);
-}
+//     var audio = {
+//         "name" : "PlayFiles",
+//         "ids" :  ids,
+//         "locale" : "en-US"
+//     }
+//     responsedata['Instructions'].push(audio);
+// }
 
-function tellTime() {
-    responsedata = {
-        "Instructions": [],
-        "Action": {}
-    };
+// function tellTime() {
+//     responsedata = {
+//         "Instructions": [],
+//         "Action": {}
+//     };
     
-    var myDate = new Date();
-    var myHour = myDate.getHours();
-    var myMinute = myDate.getMinutes();
-    var minuteDivBy5 = myMinute / 5;
-    var roundMinuteDivBy5 = Math.round(minuteDivBy5);
-    myHour = myHour % 12 || 12; // Convert hour to 12 hour day, 1-12
+//     var myDate = new Date();
+//     var myHour = myDate.getHours();
+//     var myMinute = myDate.getMinutes();
+//     var minuteDivBy5 = myMinute / 5;
+//     var roundMinuteDivBy5 = Math.round(minuteDivBy5);
+//     myHour = myHour % 12 || 12; // Convert hour to 12 hour day, 1-12
 
-    // Array of speech, starting with intro. Further sounds will be added to the end.
-    makeAudioObject("intro");
+//     // Array of speech, starting with intro. Further sounds will be added to the end.
+//     makeAudioObject("intro");
 
-    // If it's not on the hour...
-    if (myMinute > 0) {
+//     // If it's not on the hour...
+//     if (myMinute > 0) {
 
-        // If not on a 5 min div, say "just left" or "coming up for"
-        if (minuteDivBy5 != roundMinuteDivBy5) {
-            if (roundMinuteDivBy5 < minuteDivBy5) {
-                makeAudioObject("justleft");
-                // console.log("justleft")
-            } else {
-                makeAudioObject("comingupfor");
-                // console.log("comingupfor")
-            }
-        }
+//         // If not on a 5 min div, say "just left" or "coming up for"
+//         if (minuteDivBy5 != roundMinuteDivBy5) {
+//             if (roundMinuteDivBy5 < minuteDivBy5) {
+//                 makeAudioObject("justleft");
+//                 // console.log("justleft")
+//             } else {
+//                 makeAudioObject("comingupfor");
+//                 // console.log("comingupfor")
+//             }
+//         }
 
-        // What minute? Past or from?
-        if (roundMinuteDivBy5 == 6) { // if half past
-            makeAudioObject("half");
-            makeAudioObject("past");
-            // console.log("half past");
-        } else if (roundMinuteDivBy5 == 3) {
-            makeAudioObject("quarter");
-            makeAudioObject("past");
-            // console.log("quarter past");
-        } else if (roundMinuteDivBy5 == 9) {
-            makeAudioObject("quarter");
-            makeAudioObject("to");
-            // console.log("quarter to");
-        } else if (roundMinuteDivBy5 > 0 && roundMinuteDivBy5 < 7) {
-            makeAudioObject((roundMinuteDivBy5 * 5) + "m");
-            makeAudioObject("past");
-            // console.log((roundMinuteDivBy5 * 5) + "m past");
-        } else if (roundMinuteDivBy5 >= 7 && roundMinuteDivBy5 < 12) {
-            makeAudioObject((60 - (roundMinuteDivBy5 * 5)) + "m");
-            makeAudioObject("to");
-            // console.log("etc");
-        }
+//         // What minute? Past or from?
+//         if (roundMinuteDivBy5 == 6) { // if half past
+//             makeAudioObject("half");
+//             makeAudioObject("past");
+//             // console.log("half past");
+//         } else if (roundMinuteDivBy5 == 3) {
+//             makeAudioObject("quarter");
+//             makeAudioObject("past");
+//             // console.log("quarter past");
+//         } else if (roundMinuteDivBy5 == 9) {
+//             makeAudioObject("quarter");
+//             makeAudioObject("to");
+//             // console.log("quarter to");
+//         } else if (roundMinuteDivBy5 > 0 && roundMinuteDivBy5 < 7) {
+//             makeAudioObject((roundMinuteDivBy5 * 5) + "m");
+//             makeAudioObject("past");
+//             // console.log((roundMinuteDivBy5 * 5) + "m past");
+//         } else if (roundMinuteDivBy5 >= 7 && roundMinuteDivBy5 < 12) {
+//             makeAudioObject((60 - (roundMinuteDivBy5 * 5)) + "m");
+//             makeAudioObject("to");
+//             // console.log("etc");
+//         }
 
-    }
+//     }
 
-    // What hour to say? If less than 33 mins, refer to this hour. More, refer to next hour.
-    if (myMinute < 33) {
-        makeAudioObject(myHour + "h");
-        // console.log(myHour + "h");
-    } else {
-        if (myHour < 12) {
-            makeAudioObject(myHour + 1 + "h");
-            // console.log(myHour + 1 + "h");
-        } else {
-            makeAudioObject("1h");
-            // console.log("1h");
-        }
-    }
-}
+//     // What hour to say? If less than 33 mins, refer to this hour. More, refer to next hour.
+//     if (myMinute < 33) {
+//         makeAudioObject(myHour + "h");
+//         // console.log(myHour + "h");
+//     } else {
+//         if (myHour < 12) {
+//             makeAudioObject(myHour + 1 + "h");
+//             // console.log(myHour + 1 + "h");
+//         } else {
+//             makeAudioObject("1h");
+//             // console.log("1h");
+//         }
+//     }
+// }
