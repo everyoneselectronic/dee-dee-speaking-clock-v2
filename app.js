c01649e8d616401cb0e6d62c41efde7e53c0959a
everyoneselectronic <fogertyliam@gmail.com>
2018-01-31 21:13:32 +0000

    add keep awake
    keep awake

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // add requires
-// var https = require('https');
+var https = require('https');
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('./routes/sinch');
@@ -29,6 +29,7 @@ module.exports = app;
 app.listen(port);
 
 // pings server every 15 minutes to prevent dynos from sleeping
-// setInterval(() => {
-//  https.get('https://dee-dee-speaking-clock-v2.herokuapp.com/');
-// }, 900000);
+setInterval(() => {
+ https.get('https://dee-dee-speaking-clock-v2.herokuapp.com/');
+ console.log("ping");
+}, 900000);
