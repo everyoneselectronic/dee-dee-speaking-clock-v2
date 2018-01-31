501bdad58ba4a4f54ede7e26fd2636bfb8ff3afb
everyoneselectronic <fogertyliam@gmail.com>
2018-01-31 21:10:34 +0000

    all audio
    test

--- routes/sinch.js
+++ routes/sinch.js
@@ -12,7 +12,31 @@ var svamlResponse =
             },
             {
                 "name" : "PlayFiles",
-                "ids" :  [ "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/2h.wav","#href[https://dee-dee-speaking-clock-v2.herokuapp.com/2h.wav" ],
+                "ids" :  [ 
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/1h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/2h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/3h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/4h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/5h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/6h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/7h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/8h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/9h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/10h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/11h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/12h.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/5m.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/10m.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/20m.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/25m.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/comingupfor.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/half.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/intro.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/justleft.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/past.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/quarter.wav]",
+                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/to.wav]"
+                ],
                 "locale" : "en-US"
             }
         ],
