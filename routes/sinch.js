9b473afee103be73393c61d00b7a60c9e0c04196
everyoneselectronic <fogertyliam@gmail.com>
2018-02-01 21:15:17 +0000

    real

--- routes/sinch.js
+++ routes/sinch.js
@@ -2,7 +2,10 @@
 var express = require('express');
 var router = express.Router();
 
-var svamlResponse =
+var baseURL = "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/";
+var endURL = ".wav]";
+
+var svamlResponseTest =
     {
         instructions: [
             {
@@ -45,9 +48,106 @@ var svamlResponse =
         }
     };
 
+var svamlResponse =
+    {
+        instructions: [
+            {
+                "name" : "PlayFiles",
+                "ids" :  [],
+                "locale" : "en-US"
+            },
+            {
+                "name": "Say",
+                "text": "  Brought to you by everyone's electronic dot co dot U K",
+                "locale": "en-US"
+            },
+        ],
+        action: {
+             "name" : "Hangup"
+        }
+    };
+
 router.post('/', function (req, res, next) {
+    console.log(req.body.cli);
     // add tell
+    tellTime();
     //send back the response.
     res.json(svamlResponse);
 });
 module.exports = router;
+
+
+function makeAudioURL(a) {
+    var URL = baseUrl + a + endURL;
+    audioIDs.push(URL)
+};
+
+function tellTime() {
+    var audioIDs = new Array();
+
+    var myDate = new Date();
+    var myHour = myDate.getHours();
+    var myMinute = myDate.getMinutes();
+    var minuteDivBy5 = myMinute / 5;
+    var roundMinuteDivBy5 = Math.round(minuteDivBy5);
+    myHour = myHour % 12 || 12; // Convert hour to 12 hour day, 1-12
+
+    // Array of speech, starting with intro. Further sounds will be added to the end.
+    makeAudioURL("intro");
+
+    // If it's not on the hour...
+    if (myMinute > 0) {
+
+        // If not on a 5 min div, say "just left" or "coming up for"
+        if (minuteDivBy5 != roundMinuteDivBy5) {
+            if (roundMinuteDivBy5 < minuteDivBy5) {
+                makeAudioURL("justleft");
+                // console.log("justleft")
+            } else {
+                makeAudioURL("comingupfor");
+                // console.log("comingupfor")
+            }
+        }
+
+        // What minute? Past or from?
+        if (roundMinuteDivBy5 == 6) { // if half past
+            makeAudioURL("half");
+            makeAudioURL("past");
+            // console.log("half past");
+        } else if (roundMinuteDivBy5 == 3) {
+            makeAudioURL("quarter");
+            makeAudioURL("past");
+            // console.log("quarter past");
+        } else if (roundMinuteDivBy5 == 9) {
+            makeAudioURL("quarter");
+            makeAudioURL("to");
+            // console.log("quarter to");
+        } else if (roundMinuteDivBy5 > 0 && roundMinuteDivBy5 < 7) {
+            makeAudioURL((roundMinuteDivBy5 * 5) + "m");
+            makeAudioURL("past");
+            // console.log((roundMinuteDivBy5 * 5) + "m past");
+        } else if (roundMinuteDivBy5 >= 7 && roundMinuteDivBy5 < 12) {
+            makeAudioURL((60 - (roundMinuteDivBy5 * 5)) + "m");
+            makeAudioURL("to");
+            // console.log("etc");
+        }
+
+    }
+
+    // What hour to say? If less than 33 mins, refer to this hour. More, refer to next hour.
+    if (myMinute < 33) {
+        makeAudioURL(myHour + "h");
+        // console.log(myHour + "h");
+    } else {
+        if (myHour < 12) {
+            makeAudioURL(myHour + 1 + "h");
+            // console.log(myHour + 1 + "h");
+        } else {
+            makeAudioURL("1h");
+            // console.log("1h");
+        }
+    }
+
+    // add IDs to response
+    svamlResponse['Instructions']['ids'].push(audioIDs);
+};
