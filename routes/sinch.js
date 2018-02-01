9a08542abf4c0ad0d5c63892b53a43e850112933
everyoneselectronic <fogertyliam@gmail.com>
2018-02-01 22:11:27 +0000

    finial

--- routes/sinch.js
+++ routes/sinch.js
@@ -5,49 +5,6 @@ var router = express.Router();
 var baseURL = "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/";
 var endURL = ".wav]";
 
-var svamlResponseTest =
-    {
-        instructions: [
-            {
-                "name": "Say",
-                "text": "The Limmy Clock is undergoing maintenance",
-                "locale": "en-US"
-            },
-            {
-                "name" : "PlayFiles",
-                "ids" :  [ 
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/1h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/2h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/3h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/4h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/5h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/6h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/7h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/8h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/9h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/10h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/11h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/12h.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/5m.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/10m.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/20m.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/25m.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/comingupfor.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/half.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/intro.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/justleft.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/past.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/quarter.wav]",
-                    "#href[https://dee-dee-speaking-clock-v2.herokuapp.com/to.wav]"
-                ],
-                "locale" : "en-US"
-            }
-        ],
-        action: {
-             "name" : "Hangup"
-        }
-    };
-
 var svamlResponse;
 
 router.post('/', function (req, res, next) {
@@ -130,7 +87,9 @@ function tellTime() {
             // console.log("1h");
         }
     }
-
+    
+    makeAudioURL(audioIDs,"outro");
+    
     // return IDs to response
     return audioIDs;
 };
@@ -145,12 +104,7 @@ function makeSvamlResponse() {
                     "name" : "PlayFiles",
                     "ids" :  aIDs,
                     "locale" : "en-US"
-                },
-                {
-                    "name": "Say",
-                    "text": "  Brought to you by everyone's electronic dot co dot U K",
-                    "locale": "en-US"
-                },
+                }
             ],
             action: {
                  "name" : "Hangup"
